2227da893b2c3cabb01c5cd802f43a4d97da09ec
Kendrick Barrios <kkbarrios@uamv.edu.ni>
2023-10-24 23:20:06 -0600

    Separacion de la funcion crear perfil en el archivo de cabecera gestionperfil.h, creacion de la funcion encriptar, creacion de la funcion modificar perfil

--- GFP.cpp
+++ GFP.cpp
@@ -1,27 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <locale.h>
+#include <cstring>
 #include <string>
 #include <vector>
-#include <regex>
-
-void crearperfil () {
-    std::string user, password;
-    std::regex patronUser("^[a-zA-Z0-9_]+$"); 
-    std::regex patronPassword("^[\\w\\d\\s!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~]+$");
-    do {
-        system("cls");
-        std::cout << "\n*El nombre solo debe contener letras, numeros o guion bajo ( \"_\" ).*\n"
-        << "\nIngrese el nombre de usuario: ";
-        std::getline(std::cin >> std::ws, user);
-        std::cout << "\nLa contrasena puede incluir simbolos.\n"
-        << "\nIngrese la contrasena: ";
-        std::getline(std::cin >> std::ws, password);
-        if (std::regex_match(user, patronUser) && std::regex_match(password, patronPassword)) {
-            break;
-        }
-    } while (true);
-}
+#include "gestionperfil.h"
 
 int main () {
     setlocale(LC_ALL, "spanish");
